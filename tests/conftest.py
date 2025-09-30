d00bee9b2bbc3bc69d5ee281fd2ceb6b81943c23
Jakob Guenther <58778456+jagunther@users.noreply.github.com>
2025-09-30 13:40:47 +0200

    * conftest.py MOD integrals_h2o() MOD integrals_random() NEW integrals_random_physicist() NEW hamil_h2o() NEW hamil_random() NEW hamil_random_pair()

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,50 +3,78 @@ from pyscf import gto, scf
 from pyscf.mcscf import CASCI
 from pyscf.ao2mo import restore
 import numpy as np
+from champy.ElectronicStructure import ElectronicStructure
 
 
-@pytest.fixture(scope="session")
-def integrals_h2o(request):
+def _integrals_h2o(num_orb: int, num_elec: int):
     xyz_H2O = """
     O          0.00000        0.00000        0.11779
     H          0.00000        0.75545       -0.47116
     H          0.00000       -0.75545       -0.47116
     """
-    n = request.param
     mol = gto.M(atom=xyz_H2O, basis="sto3g")
     hf = scf.RHF(mol).newton()
     hf.run()
-    casci = CASCI(hf, n, n)
+    casci = CASCI(hf, num_orb, num_elec)
     h1e, h0 = casci.get_h1eff()
-    h2e = restore("s1", casci.get_h2eff(), n)
+    h2e = restore("s1", casci.get_h2eff(), num_orb)
     return h0, h1e, h2e
+
+
+def _integrals_random(num_orb, order):
+    assert order in ["chemist", "physicist"]
+    np.random.seed(4040)
+    n = num_orb
+    h1e = np.random.rand(n, n)
+    h1e += np.einsum("pq -> qp", h1e)
+
+    h2e = np.random.rand(n, n, n, n)
+    if order == "chemist":
+        h2e += np.einsum("pqrs -> qprs", h2e)
+        h2e += np.einsum("pqrs -> pqsr", h2e)
+        h2e += np.einsum("pqrs -> rspq", h2e)
+    else:
+        h2e += np.einsum("pqrs -> sqrp", h2e)
+        h2e += np.einsum("pqrs -> prqs", h2e)
+        h2e += np.einsum("pqrs -> qpsr", h2e)
+    return 0, h1e, h2e
+
+
+@pytest.fixture(scope="session")
+def integrals_h2o(request):
+    return _integrals_h2o(request.param, request.param)
 
 
 @pytest.fixture(scope="session")
 def integrals_random(request):
-    np.random.seed(4040)
-    n = request.param
-    h1e = np.random.rand(n, n)
-    h1e += np.einsum("pq -> qp", h1e)
-
-    h2e = np.random.rand(n, n, n, n)
-    h2e += np.einsum("pqrs -> qprs", h2e)
-    h2e += np.einsum("pqrs -> pqsr", h2e)
-    h2e += np.einsum("pqrs -> rspq", h2e)
-
-    return 0, h1e, h2e
+    return _integrals_random(request.param, "chemist")
 
 
 @pytest.fixture(scope="session")
 def integrals_random_physicist(request):
-    np.random.seed(4040)
-    n = request.param
-    h1e = np.random.rand(n, n)
-    h1e += np.einsum("pq -> qp", h1e)
+    return _integrals_random(request.param, "physicist")
 
-    h2e = np.random.rand(n, n, n, n)
-    h2e += np.einsum("pqrs -> sqrp", h2e)
-    h2e += np.einsum("pqrs -> prqs", h2e)
-    h2e += np.einsum("pqrs -> qpsr", h2e)
 
-    return 0, h1e, h2e
+@pytest.fixture(scope="session")
+def hamil_h2o(request):
+    num_orb = request.param[0]
+    num_elec = request.param[1]
+    h0, h1e, h2e = _integrals_h2o(num_orb, num_elec)
+    return ElectronicStructure(h0, h1e, h2e, num_elec)
+
+
+@pytest.fixture(scope="session")
+def hamil_random(request):
+    num_orb = request.param[0]
+    num_elec = request.param[1]
+    h0, h1e, h2e = _integrals_random(num_orb, "chemist")
+    return ElectronicStructure(h0, h1e, h2e, num_elec)
+
+
+@pytest.fixture(scope="session")
+def hamil_random_pair(request):
+    h0_1, h1e_1, h2e_1 = _integrals_random(request.param["num_orb1"], "chemist")
+    h0_2, h1e_2, h2e_2 = _integrals_random(request.param["num_orb2"], "chemist")
+    hamil_1 = ElectronicStructure(h0_1, h1e_1, h2e_1, request.param["num_elec1"])
+    hamil_2 = ElectronicStructure(h0_2, h1e_2, h2e_2, request.param["num_elec2"])
+    return hamil_1, hamil_2
