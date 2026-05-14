b5c0486968b537e02b40039f626a133c69a5a05f
Jakob Guenther <jakobg@mit.edu>
2026-05-14 17:31:52 -0400

    default enable jax float 64

--- champy/__init__.py
+++ champy/__init__.py
@@ -1,3 +1,6 @@
 from .ElectronicStructure import ElectronicStructure
 from .MajoranaPair import MajoranaPair
 from .PauliHamiltonian import PauliHamiltonian
+import jax
+
+jax.config.update("jax_enable_x64", True)
